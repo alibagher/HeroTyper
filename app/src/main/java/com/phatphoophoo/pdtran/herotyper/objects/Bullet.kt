b5112fab46418aaa2d69db78267f8afc9ac23f34
alibagher <a6bagher@uwaterloo.ca>
2020-02-23 17:29:43 -0500

    rockets show up

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/objects/Bullet.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/objects/Bullet.kt
@@ -2,8 +2,8 @@ package com.phatphoophoo.pdtran.herotyper.objects
 
 class Bullet(override var position: Pair<Float, Float>, override var velocity: Float) : GameObject {
     override val direction: Direction = Direction.UP
-    override val height: Float = 10f
-    override val width: Float = 10f
+    override val height: Float = 100f
+    override val width: Float = 100f
 
     override fun updatePosition() {
         this.position = Pair(position.first, position.second - velocity)
--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/GameScreenView.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/GameScreenView.kt
@@ -20,7 +20,12 @@ class GameScreenView : View {
     private var cnv: Canvas? = null
 
     private var player : Player = Player(Pair(0f,0f))
-    private var playerPos : Pair<Float, Float> = Pair(0f,0f)
+//    private var playerPos : Pair<Float, Float> = Pair(0f,0f)
+
+    private var bullet: Bitmap? = null
+    private var bulletPosList: MutableList<Pair<Float, Float>> = mutableListOf()
+    private var bulletBmList: MutableList<Bitmap> = mutableListOf()
+    private var bulletSzList: MutableList<Pair<Float, Float>> = mutableListOf()
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -32,6 +37,7 @@ class GameScreenView : View {
     init {
         spaceship = BitmapFactory.decodeResource(resources, R.drawable.spaceship)
         enemy = BitmapFactory.decodeResource(resources, R.drawable.meteor)
+        bullet = BitmapFactory.decodeResource(resources, R.drawable.rocket)
 //        this.update(c)
     }
 
@@ -43,7 +49,14 @@ class GameScreenView : View {
 
         player = modelStream.player
 
-        Log.i("palayer first pos", player.position.first.toString())
+        var lob = modelStream.bullets.toMutableList()
+        bulletPosList = lob.map { bullet -> bullet.position }.toMutableList()
+        bulletSzList = lob.map { bullet -> Pair(bullet.width, bullet.height) }.toMutableList()
+
+
+
+
+        Log.i("palayer first pos", player.position.first.toString() + " " + player.position.second.toString())
 
         this.invalidate()
     }
@@ -57,7 +70,7 @@ class GameScreenView : View {
         var bottomY: Float = bounds.bottom - spaceshipY
 
 
-        playerPos = Pair(player?.position.first, player?.position.second)
+        //playerPos = Pair(player?.position.first, player?.position.second)
 
 
         spaceship = Bitmap.createScaledBitmap(spaceship, spaceshipX, spaceshipY, false)
@@ -65,11 +78,22 @@ class GameScreenView : View {
         for ((szX, szY) in enemySzList) {
             enemyBmList.add(Bitmap.createScaledBitmap(enemy, szX.toInt(), szY.toInt(), false))
         }
+        bulletBmList.clear()
+        for ((szX, szY) in bulletSzList) {
+            bulletBmList.add(Bitmap.createScaledBitmap(bullet, szX.toInt(), szY.toInt(), false))
+        }
 
-        c?.drawBitmap(spaceship, playerPos.first, bottomY, null)
+
+        c?.drawBitmap(spaceship, player?.position.first, bottomY + 10, null)
         enemyPosList.forEachIndexed{i, (posX, posY) ->
             c?.drawBitmap(enemyBmList[i], posX, posY, null)
         }
+        bulletPosList.forEachIndexed{i, (posX, posY) ->
+            c?.drawBitmap(bulletBmList[i], posX, posY - spaceshipY - 500, null)
+            Log.i("bullet position:", posX.toString()+ " " + posY.toString())
+        }
+
+
 
     }
 }
