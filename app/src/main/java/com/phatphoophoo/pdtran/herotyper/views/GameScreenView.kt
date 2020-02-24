1d978edbf684b6ce12a178e30125dc408b412aca
Roland Li <roland_li@hotmail.ca>
2020-02-23 23:29:50 -0500

    Score and live print

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/GameScreenView.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/GameScreenView.kt
@@ -2,6 +2,7 @@ package com.phatphoophoo.pdtran.herotyper.views
 
 import android.content.Context
 import android.graphics.*
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import com.phatphoophoo.pdtran.herotyper.R
@@ -17,40 +18,45 @@ class GameScreenView : View {
     private var cnv: Canvas? = null
 
     private var player : Player = Player(Pair(0f,0f))
-//    private var playerPos : Pair<Float, Float> = Pair(0f,0f)
 
     private var bulletBitmap: Bitmap? = null
     private var bulletPosList: MutableList<Pair<Float, Float>> = mutableListOf()
     private var bulletBmList: MutableList<Bitmap> = mutableListOf()
     private var bulletSzList: MutableList<Pair<Float, Float>> = mutableListOf()
 
+    private var score: Int = 0
+    private val scoreText = "Score: "
+    private var lives: Int = 0
+    private val livesText = "Lives: "
+    private val textPaint = TextPaint()
+
     constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        var paint = Paint()
-        paint.textAlign = Paint.Align.CENTER
-    }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, attributeSetId: Int) : super(context, attrs, attributeSetId)
 
     init {
         spaceshipBitmap = BitmapFactory.decodeResource(resources, R.drawable.spaceship)
         enemyBitmap = BitmapFactory.decodeResource(resources, R.drawable.meteor)
         bulletBitmap = BitmapFactory.decodeResource(resources, R.drawable.rocket)
-//        this.update(c)
+
+        textPaint.textSize = 42f
+        textPaint.color = Color.WHITE
     }
 
-    fun setModel(modelStream: GameScreenModel) {
+    fun setModel(model: GameScreenModel) {
         // TODO: Add logic to fetch enemyBitmap list
-        var loe = modelStream.enemies.toMutableList()
+        var loe = model.enemies.toMutableList()
         enemyPosList = loe.map { enemy -> enemy.position }.toMutableList()
         enemySzList = loe.map { enemy -> Pair(enemy.width, enemy.height) }.toMutableList()
 
-        player = modelStream.player
+        player = model.player
 
-        var lob = modelStream.bullets.toMutableList()
+        var lob = model.bullets.toMutableList()
         bulletPosList = lob.map { bullet -> bullet.position }.toMutableList()
         bulletSzList = lob.map { bullet -> Pair(bullet.width, bullet.height) }.toMutableList()
 
-       //  Log.i("palayer first pos", player.position.first.toString() + " " + player.position.second.toString())
+        score = model.score
+        lives = model.lives
 
         this.invalidate()
     }
@@ -58,10 +64,6 @@ class GameScreenView : View {
     override fun onDraw(c: Canvas?) {
         super.onDraw(c)
         if (c != null && cnv == null) cnv = c
-        // Log.i("current canvas", cnv.toString())
-        var bounds = RectF(c?.clipBounds)
-
-        //playerPos = Pair(player?.position.first, player?.position.second)
 
         spaceshipBitmap = Bitmap.createScaledBitmap(spaceshipBitmap, player.width.toInt(), player.height.toInt(), false)
         enemyBmList.clear()
@@ -79,7 +81,9 @@ class GameScreenView : View {
         }
         bulletPosList.forEachIndexed{i, (posX, posY) ->
             c?.drawBitmap(bulletBmList[i], posX, posY, null)
-            // Log.i("bulletBitmap position:", posX.toString()+ " " + posY.toString())
         }
+
+        c?.drawText("$scoreText$score", 50f, 50f, textPaint)
+        c?.drawText("$livesText$lives", 50f, 100f, textPaint)
     }
 }
