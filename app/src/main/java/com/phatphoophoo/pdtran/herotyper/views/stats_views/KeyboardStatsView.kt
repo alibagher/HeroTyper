205465a22f620885511486513042ca7460ec52de
Zain Khan <zain@Khutajas-MBP.lan>
2020-03-25 17:32:39 -0400

    WIP: Initial keyboard stats view done

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/KeyboardStatsView.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/KeyboardStatsView.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.phatphoophoo.pdtran.herotyper.R
+import kotlin.random.Random
 
 class KeyboardStatsView:
     LinearLayout {
@@ -14,23 +15,40 @@ class KeyboardStatsView:
     constructor(context: Context, attrs: AttributeSet?, attributeSetId: Int) : super(context, attrs, attributeSetId)
 
     val ANALYSIS_THRESHOLD_COUNT = 30
+    val keyButtonIds: IntArray = intArrayOf(R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+        R.id.button8, R.id.button9, R.id.button10, R.id.button11, R.id.button12, R.id.button13, R.id.button14, R.id.button15,
+        R.id.button16, R.id.button17, R.id.button18, R.id.button19, R.id.button20, R.id.button21, R.id.button22,R.id.button23,
+        R.id.button24, R.id.button25, R.id.button26)
 
     init {
         inflate(context, R.layout.custom_keyboard_stats_layout, this)
-        val mainLayout = findViewById<LinearLayout>(R.id.custom_keyboard_keys_medium)
-        mainLayout.setBackgroundColor(ContextCompat.getColor(this.context, R.color.colorWhite))
+
+        //testing
+        initWithFakeData()
     }
 
-    //Place key accuracy
-    fun initKeyColors(keyStats: Map<String, Pair<Int, Int>>) {
-        val keyButtonIds: IntArray = intArrayOf(R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7,
-            R.id.button8, R.id.button9, R.id.button10, R.id.button11, R.id.button12, R.id.button13, R.id.button14, R.id.button15,
-            R.id.button16, R.id.button17, R.id.button18, R.id.button19, R.id.button20, R.id.button21, R.id.button22,R.id.button23,
-            R.id.button24, R.id.button25, R.id.button26)
+    //TODO: Remove later
+    private fun initWithFakeData(){
 
+        //Generate fake data
+        val keyStats =  mutableMapOf<String, Pair<Int, Int>>()
         for(keyButtonId in keyButtonIds) {
             val button = findViewById<Button>(keyButtonId)
-            val key = button.text
+            val key = button.text.toString()
+            val hits = Random.nextInt(0, 100)
+            val misses = Random.nextInt(0, 100)
+
+            keyStats[key] = Pair(hits, misses)
+        }
+
+        //Initialize using fake data
+        initKeyColors(keyStats)
+    }
+    //Place key accuracy
+    fun initKeyColors(keyStats: Map<String, Pair<Int, Int>>) {
+        for(keyButtonId in keyButtonIds) {
+            val button = findViewById<Button>(keyButtonId)
+            val key = button.text.toString()
             val hitMissPair = keyStats[key]
             val total = hitMissPair!!.first + hitMissPair!!.second
             val ratio = hitMissPair!!.first / hitMissPair!!.second
@@ -38,13 +56,13 @@ class KeyboardStatsView:
 
             if(total < ANALYSIS_THRESHOLD_COUNT) {
                 // if the key has not been pressed enough times for analysis
-                button.setBackgroundColor(ContextCompat.getColor(this.context, R.color.colorDisable))
+                button.setBackgroundResource(R.drawable.button_shape_neutral)
             } else if(ratio > 2) {
-                button.setBackgroundColor(ContextCompat.getColor(this.context, R.color.colorPrimary))
+                button.setBackgroundResource(R.drawable.button_shape_green)
             } else if(ratio > 1) {
-                button.setBackgroundColor(ContextCompat.getColor(this.context, R.color.colorWarn))
+                button.setBackgroundResource(R.drawable.button_shape_yellow)
             } else {
-                button.setBackgroundColor(ContextCompat.getColor(this.context, R.color.redOverlay))
+                button.setBackgroundResource(R.drawable.button_shape_red)
             }
 
         }
