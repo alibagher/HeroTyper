38b63a003d3bd6a9546de7f136e4821cb95a4d04
Zain Khan <zain@Khutajas-MBP.lan>
2020-03-27 17:22:07 -0400

    WIP: added create graph functionality

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/SpeedStatsView.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/SpeedStatsView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.phatphoophoo.pdtran.herotyper.R
+import lecho.lib.hellocharts.formatter.SimpleAxisValueFormatter
 import lecho.lib.hellocharts.model.Axis
 import lecho.lib.hellocharts.model.Line
 import lecho.lib.hellocharts.model.LineChartData
@@ -16,29 +17,32 @@ class SpeedStatsView: LinearLayout {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, attributeSetId: Int) : super(context, attrs, attributeSetId)
 
+    private val data: LineChartData = LineChartData()
+
     init {
 
         inflate(context, R.layout.speed_stats_layout, this)
-
-        val values = mutableListOf<PointValue>()
-        values.add(PointValue(0F, 2F))
-        values.add(PointValue(1F, 4F))
-        values.add(PointValue(2F, 3F))
-        values.add(PointValue(3F, 4F))
-
-        val line = Line(values).setColor(R.color.colorPrimaryDark).setCubic(true)
-        val lines = arrayListOf<Line>(line)
-
-        val data = LineChartData()
-        data.lines = lines
 
         val axisX = Axis()
         val axisY = Axis().setHasLines(true)
         axisX.name = "Game Number"
         axisY.name = "WPM"
+
         data.axisXBottom = axisX
         data.axisYLeft = axisY
 
+        //TODO: Remove later
+        createGraph(listOf(20,32,43,56, 20,32,43,56))
+    }
+
+    fun createGraph(speedStats: List<Int>) {
+        val points = speedStats.mapIndexed{idx, value -> PointValue(idx.toFloat() + 1, value.toFloat())}
+
+        val line = Line(points)
+        line.color = R.color.colorPrimary
+        val lines = arrayListOf(line)
+
+        data.lines = lines
 
         val chart = findViewById<LineChartView>(R.id.chart)
         chart.lineChartData = data
