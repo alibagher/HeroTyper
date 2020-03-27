7109f415b48506a296996c9c3ba8e8ff2b94597b
Zain Khan <zain@Khutajas-MBP.lan>
2020-03-27 16:27:24 -0400

    WIP: shifted some keyboard logic to presenter

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/activities/StatsActivity.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/activities/StatsActivity.kt
@@ -3,11 +3,15 @@ package com.phatphoophoo.pdtran.herotyper.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.phatphoophoo.pdtran.herotyper.R
+import com.phatphoophoo.pdtran.herotyper.presenters.StatsPresenter
+import kotlinx.android.synthetic.main.activity_stats.keyboard_stats_view
 
 class StatsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
+
+        val statsPresenter = StatsPresenter(this, keyboard_stats_view)
     }
 }
--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/KeyboardStatsView.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/views/stats_views/KeyboardStatsView.kt
@@ -5,8 +5,9 @@ import android.graphics.LightingColorFilter
 import android.util.AttributeSet
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.phatphoophoo.pdtran.herotyper.R
-import kotlin.random.Random
+import java.util.*
 
 class KeyboardStatsView:
     LinearLayout {
@@ -22,37 +23,41 @@ class KeyboardStatsView:
 
     //TODO: change later
     //Colors
-    private val softGray = 0xBDB1B1
-    private val softGreen = 0x87F09C
-    private val softYellow = 0xEDF087
-    private val softRed = 0xF08787
+    enum class KeyColor(val value: Int) {
+        softGray(0xBDB1B1),
+        softGreen(0x87F09C),
+        softYellow(0xEDF087),
+        softRed(0xF08787)
+    }
+
+    enum class KeyStatus(val value: String) {
+        UNKNOWN("N/A"),
+        GOOD("Good"),
+        WARN("Okay"),
+        BAD("Bad")
+    }
 
     init {
         inflate(context, R.layout.custom_keyboard_stats_layout, this)
-
-        //TODO: Remove later
-        //testing
-        Thread.sleep(1000) //To prevent app crash for now
-        initWithFakeData()
     }
 
-    //TODO: Remove later
-    private fun initWithFakeData(){
+    fun showDetailedKeyStats(key: String, hitMissPair: Pair<Int, Int>, keyStatus: KeyStatus) {
 
-        //Generate fake data
-        val keyStats =  mutableMapOf<String, Pair<Int, Int>>()
-        for(keyButtonId in keyButtonIds) {
-            val button = findViewById<Button>(keyButtonId)
-            val key = button.text.toString()
-            val hits = Random.nextInt(0, 100)
-            val misses = Random.nextInt(0, 100)
+        val keyTextView = findViewById<TextView>(R.id.key_value)
+        val statusTextView = findViewById<TextView>(R.id.key_status)
+        val hitsTextView = findViewById<TextView>(R.id.key_hit)
+        val missesTextView = findViewById<TextView>(R.id.key_miss)
 
-            keyStats[key] = Pair(hits, misses)
-        }
+        keyTextView.text = String.format(resources.getString(R.string.keyboard_stats_key_value),
+            key.toUpperCase(Locale.getDefault()))
+        statusTextView.text = String.format(resources.getString(R.string.keyboard_stats_key_status), keyStatus.value)
+        hitsTextView.text = String.format(resources.getString(R.string.keyboard_stats_key_hits), hitMissPair.first)
+        missesTextView.text = String.format(resources.getString(R.string.keyboard_stats_key_misses, hitMissPair.second))
 
-        //Initialize using fake data
-        initKeyColors(keyStats)
+
     }
+
+    //TODO move logic to presenter with key status
     //Place key accuracy
     fun initKeyColors(keyStats: Map<String, Pair<Int, Int>>) {
         for(keyButtonId in keyButtonIds) {
@@ -65,13 +70,13 @@ class KeyboardStatsView:
 
             if(total < ANALYSIS_THRESHOLD_COUNT) {
                 // if the key has not been pressed enough times for analysis
-                button.background.colorFilter = LightingColorFilter(softGray, 0)
+                button.background.colorFilter = LightingColorFilter(KeyColor.softGray.value, 0)
             } else if(ratio > 2) {
-                button.background.colorFilter = LightingColorFilter(softGreen, 0)
+                button.background.colorFilter = LightingColorFilter(KeyColor.softGreen.value, 0)
             } else if(ratio > 1) {
-                button.background.colorFilter = LightingColorFilter(softYellow, 0)
+                button.background.colorFilter = LightingColorFilter(KeyColor.softYellow.value, 0)
             } else {
-                button.background.colorFilter = LightingColorFilter(softRed, 0)
+                button.background.colorFilter = LightingColorFilter(KeyColor.softRed.value, 0)
             }
 
         }
