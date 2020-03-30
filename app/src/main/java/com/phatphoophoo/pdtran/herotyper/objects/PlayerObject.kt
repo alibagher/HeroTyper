de3eb2dbc8080c7565dd0b0c539d2d491552e453
Roland Li <roland_li@hotmail.ca>
2020-03-30 16:59:40 -0400

    Rename player to fit convention

--- app/src/main/java/com/phatphoophoo/pdtran/herotyper/objects/PlayerObject.kt
+++ app/src/main/java/com/phatphoophoo/pdtran/herotyper/objects/PlayerObject.kt
@@ -2,7 +2,7 @@ package com.phatphoophoo.pdtran.herotyper.objects
 
 import com.phatphoophoo.pdtran.herotyper.R
 
-class Player(override var position: Pair<Float, Float>) : GameObject() {
+class PlayerObject(override var position: Pair<Float, Float>) : GameObject() {
     override val velocity: Float = 0f
     override val height: Float = 200f
     override val width: Float = 200f
